e1f131c3ae50f6c47d9ac2e0ef1fa27218babada
Ilya Margolin <imargolin@luware.net>
2015-09-04 15:28:05 +0300

    Introduce SeqDiagPanel.Position property

--- TextToSeqDiag/TextToSeqDiag/SeqDiagPanelTests.cs
+++ TextToSeqDiag/TextToSeqDiag/SeqDiagPanelTests.cs
@@ -25,29 +25,46 @@ namespace TextToSeqDiag
         [TestMethod]
         public void Verify_One_Column()
         {
-            _view.Children.Add(CreateRect("1"));
+            _view.Children.Add(CreateRect(0));
             _view.VerifySnapshot();
         }
 
-        private static Border CreateRect(string text)
+        private static Border CreateRect(int column)
         {
-            return new Border
+            var border = new Border
             {
                 Margin = new Thickness(3),
                 BorderBrush = Brushes.Black,
                 BorderThickness = new Thickness(1),
                 Child = new TextBlock
                 {
-                    Text = text,
+                    Text = "col: " + column,
                     VerticalAlignment = VerticalAlignment.Center,
                     HorizontalAlignment = HorizontalAlignment.Center
                 }
             };
+            SeqDiagPanel.SetPosition(border, new Position { Column = column });
+            return border;
         }
     }
-
+    public sealed class Position
+    {
+        public int Column { get; set; }
+    }
     public sealed class SeqDiagPanel : Panel
     {
+        public static readonly DependencyProperty PositionProperty = DependencyProperty.RegisterAttached(
+            "Position", typeof(Position), typeof(SeqDiagPanel), new PropertyMetadata(default(Position)));
+
+        public static void SetPosition(DependencyObject element, Position value)
+        {
+            element.SetValue(PositionProperty, value);
+        }
+
+        public static Position GetPosition(DependencyObject element)
+        {
+            return (Position)element.GetValue(PositionProperty);
+        }
         protected override Size MeasureOverride(Size availableSize)
         {
             var size = default(Size);
