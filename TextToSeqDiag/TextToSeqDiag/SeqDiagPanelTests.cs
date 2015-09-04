5748a39118bfa9c7b8981d3e8e59ba0c1084fd7a
Ilya Margolin <imargolin@luware.net>
2015-09-04 15:57:47 +0300

    Verify_Second_Row

--- TextToSeqDiag/TextToSeqDiag/SeqDiagPanelTests.cs
+++ TextToSeqDiag/TextToSeqDiag/SeqDiagPanelTests.cs
@@ -27,19 +27,29 @@ namespace TextToSeqDiag
         [TestMethod]
         public void Verify_One_Column()
         {
-            _view.Children.Add(CreateRect(0));
+            _view.Children.Add(CreateRect(0, 0));
             _view.VerifySnapshot();
         }
 
         [TestMethod]
         public void Verify_Two_Columns()
         {
-            _view.Children.Add(CreateRect(0));
-            _view.Children.Add(CreateRect(1));
+            _view.Children.Add(CreateRect(0, 0));
+            _view.Children.Add(CreateRect(1, 0));
             _view.VerifySnapshot();
         }
 
-        private static Border CreateRect(int column)
+        [TestMethod]
+        public void Verify_Second_Row()
+        {
+            _view.Children.Add(CreateRect(0, 0));
+            _view.Children.Add(CreateRect(1, 0));
+            _view.Children.Add(CreateRect(0, 1));
+            _view.Children.Add(CreateRect(1, 1));
+            _view.VerifySnapshot();
+        }
+
+        private static Border CreateRect(int column, int row)
         {
             var border = new Border
             {
@@ -48,12 +58,16 @@ namespace TextToSeqDiag
                 BorderThickness = new Thickness(1),
                 Child = new TextBlock
                 {
-                    Text = "col: " + column,
+                    Text = string.Format("({0}, {1})", column, row),
                     VerticalAlignment = VerticalAlignment.Center,
                     HorizontalAlignment = HorizontalAlignment.Center
                 }
             };
-            SeqDiagPanel.SetPosition(border, new Position {Column = column});
+            SeqDiagPanel.SetPosition(border, new Position
+            {
+                Column = column, 
+                Row = row,
+            });
             return border;
         }
     }
@@ -61,6 +75,7 @@ namespace TextToSeqDiag
     public sealed class Position
     {
         public int Column { get; set; }
+        public int Row { get; set; }
     }
 
     public sealed class SeqDiagPanel : Panel
@@ -69,6 +84,7 @@ namespace TextToSeqDiag
             "Position", typeof (Position), typeof (SeqDiagPanel), new PropertyMetadata(default(Position)));
 
         private Dictionary<int, Column> _columns;
+        private Dictionary<int, Row> _rows;
 
         public static void SetPosition(DependencyObject element, Position value)
         {
@@ -83,15 +99,18 @@ namespace TextToSeqDiag
         protected override Size MeasureOverride(Size availableSize)
         {
             _columns = GroupByColumns();
+            _rows = GroupByRows();
 
-            var size = default(Size);
             foreach (UIElement child in Children)
             {
                 child.Measure(availableSize);
-                size = child.DesiredSize;
+                var size = child.DesiredSize;
                 _columns[GetPosition(child).Column].Update(size.Width);
+                _rows[GetPosition(child).Row].Update(size.Height);
             }
-            return new Size(_columns.Values.Sum(c => c.Width), size.Height);
+            return new Size(
+                _columns.Values.Sum(c => c.Width),
+                _rows.Values.Sum(c => c.Height));
         }
 
         private Dictionary<int, Column> GroupByColumns()
@@ -103,15 +122,26 @@ namespace TextToSeqDiag
                 .ToDictionary(c => c.ColumnIndex, c => c);
         }
 
+        private Dictionary<int, Row> GroupByRows()
+        {
+            return Children.OfType<UIElement>()
+                .GroupBy(c => GetPosition(c).Row)
+                .OrderBy(group => group.Key)
+                .Select(group => new Row(group))
+                .ToDictionary(c => c.RowIndex, c => c);
+        }
+
         protected override Size ArrangeOverride(Size finalSize)
         {
             UpdateColumnOffsets();
+            UpdateRowOffsets();
             foreach (UIElement child in Children)
             {
-                var column = GetPosition(child).Column;
-                child.Arrange(new Rect(
-                    new Point(_columns[column].Left, 0), 
-                    child.DesiredSize));
+                var position = GetPosition(child);
+                var topLeft = new Point(
+                    _columns[position.Column].Left,
+                    _rows[position.Row].Top);
+                child.Arrange(new Rect(topLeft, child.DesiredSize));
             }
             return base.ArrangeOverride(finalSize);
         }
@@ -123,6 +153,15 @@ namespace TextToSeqDiag
             {
                 column.Left = accumulator;
                 accumulator += column.Width;
+            }
+        }
+        private void UpdateRowOffsets()
+        {
+            var accumulator = 0.0;
+            foreach (var row in _rows.Values)
+            {
+                row.Top = accumulator;
+                accumulator += row.Height;
             }
         }
 
@@ -143,6 +182,26 @@ namespace TextToSeqDiag
             public void Update(double width)
             {
                 Width = Math.Max(width, Width);
+            }
+        }
+
+        private sealed class Row
+        {
+            public Row(IGrouping<int, UIElement> group)
+            {
+                Elements = group.ToArray();
+                RowIndex = group.Key;
+            }
+
+            public int RowIndex { get; private set; }
+
+            public UIElement[] Elements { get; private set; }
+            public double Height { get; private set; }
+            public double Top { get; set; }
+
+            public void Update(double height)
+            {
+                Height = Math.Max(height, Height);
             }
         }
     }
